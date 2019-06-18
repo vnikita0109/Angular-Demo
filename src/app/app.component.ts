09a59590750823c5560448ebd981f68323c3247b
vnikita0109 <vnikita793@gmail.com>
2019-06-18 18:01:29 +0530

    First commit of MyFoodApp with zomato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FoodService } from './food.service';
 
 @Component({
@@ -6,23 +6,23 @@ import { FoodService } from './food.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
  // providers:[FoodService]
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MyFoodApp';
   restaurants=[];
 
   constructor(private foodservice:FoodService){
 
   }
-
   ngOnInit(){
     this.foodservice.getAllRestaurants().subscribe((data:any)=>{
       console.log(data);
-      data=data.restaurants.filter(e=>{
-        return (e.restaurants.photos !== undefined)?e:null
-      })
-      console.log(data)
-      this.restaurants=data;
+      // data=data.restaurants.filter(e=>{
+      //   return (e.restaurants.photos !== undefined)?e:null;
+      // })
+      //console.log(data)
+      this.restaurants=data.restaurants;
     })
   }
 }
